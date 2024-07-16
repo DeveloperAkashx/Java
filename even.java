d625072e1d236681983f853e153ef23948123173
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2024-07-16 03:54:58 -0700

    Even Odd Program

---
+++ even.java
@@ -0,0 +1,15 @@
+class even
+{
+public static void main(String[]args)
+{
+int n = 8;
+if(n%2==0)
+{
+System.out.println("This is Even Number");
+}
+else
+{
+System.out.println("This is odd number");
+}
+}
+}
