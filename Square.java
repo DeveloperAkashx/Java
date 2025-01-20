8e21cece6a19ffd1b114b5b3d191cea7e211143c
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-01-20 17:23:30 +0530

    Calculate Perimeter and Area of Square

---
+++ Square.java
@@ -0,0 +1,9 @@
+public class Square {
+    public static void main(String[] args) {
+        double side = 5;
+        double area = side*side;
+        double perimeter = 4*side;
+        System.out.println("Area of square is: " + area);
+        System.out.println("Perimeter of square is: " + perimeter);
+    }
+}
