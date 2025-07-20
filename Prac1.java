0579e95003cc276281a20e4f7ddcdc5945cf701a
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-20 13:51:25 +0530

    Hands On

---
+++ Prac1.java
@@ -0,0 +1,19 @@
+package DSA;
+
+public class Prac1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hotel Menu :");
+		
+		System.out.println("************************************");
+		
+		System.out.println("Sr.No\t item\t Price\t");
+		
+		System.out.println("************************************");
+		
+		System.out.println(" 1.\t Paneer Fry\t 349 \n 2.\t Italian Dish\t 589 \n 3.\t South Dish\t 450\t");
+		
+	}
+
+}
