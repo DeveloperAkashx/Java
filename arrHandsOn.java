1967fed9ead772f8db8ad9126ee4eab215ac65c8
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-22 13:33:19 +0530

    Array Hands On

---
+++ arrHandsOn.java
@@ -0,0 +1,13 @@
+public class arrHandsOn {
+    public static void main(String[] args) {
+        int marks [] = new int[3];
+
+        marks[0] = 99;
+        marks[1] = 98;
+        marks[2] = 95;
+
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]+" ");
+        }
+    }
+}
